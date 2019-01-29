058fb4578bc9d414210490ba429c7124c3f478a8
CAIO DA COSTA CARMO <17259196@CFP127ALUNO.LOCAL>
2019-01-29 11:04:31 -0200

    Validação sequência número

--- app/src/main/java/com/example/caio/cadastrousuario/MainActivity.kt
+++ app/src/main/java/com/example/caio/cadastrousuario/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.caio.cadastrousuario
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,17 +17,39 @@ class MainActivity : AppCompatActivity() {
             val email = txtEmail.text.toString()
             val senha = txtSenha.text.toString()
 
+            val p = Pattern.compile("[0-9]+")
+            val m = p.matcher(senha)
+
+            val nroExtraidos = StringBuilder()
+            while (m.find()) {
+
+                nroExtraidos.append(m.group())
+
+            }
+
+            var validarSeq = false
+
+            if (nroExtraidos.length > 0){
+
+                val segundoNum = nroExtraidos.first()+1
+                val terceiroNum = segundoNum+1
+
+                if (nroExtraidos.length == 1 || nroExtraidos.length == 2)
+                    validarSeq = false
+                else
+                    if (nroExtraidos.substring(1, 2).equals(segundoNum.toString()) && nroExtraidos.substring(2, 3).equals(terceiroNum.toString()))
+                        validarSeq = true
+
+            }
+
             if (usuario.length < 3)
                 txtUsuario.error = "O usuário deve conter no mínimo 3 caracteres"
 
             if (!email.contains("@"))
                 txtEmail.error = "Email inválido! O email deve conter um @"
 
-            if (senha.length < 4 || !senha.contains(Regex("[0-9]"))){
-
+            if (senha.length < 4 || !senha.contains(Regex("[0-9]")) || validarSeq)
                 txtSenha.error = "A senha deve conter no mínimo 4 caracteres, não ser uma sequência númerica e conter ao menos um número"
-
-            }
 
         }
 
